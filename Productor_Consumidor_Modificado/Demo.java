56c13821ceb5e5d11dd209cdb0b105c78430cf1c
gopoma <gordono@unsa.edu.pe>
2024-05-05 16:45:09 -0500

    Se probó el código para el Productor-Consumidor modificado

---
+++ Productor_Consumidor_Modificado/Demo.java
@@ -0,0 +1,12 @@
+package Productor_Consumidor_Modificado;
+
+public class Demo {
+    public static void main(String[] args) {
+        CubbyHole  cub  = new CubbyHole();
+        Consumidor cons = new Consumidor(cub, 1);
+        Productor  prod = new Productor(cub, 1);
+        
+        prod.start();
+        cons.start();
+    }
+}
