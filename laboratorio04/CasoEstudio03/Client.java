0b0092f5dd0c3e4869c47ee6cc66d5c964dcd0e3
gopoma <gordono@unsa.edu.pe>
2024-05-18 23:54:54 -0500

    Se implementó el READ ALL en el Cliente

--- laboratorio04/CasoEstudio03/Client.java
+++ laboratorio04/CasoEstudio03/Client.java
@@ -3,6 +3,7 @@ package CasoEstudio03;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 
@@ -11,13 +12,69 @@ public class Client {
         try {
             CreditCardService c = (CreditCardService) Naming.lookup("rmi://localhost:4000/CreditCardService");
 
+            //? Error Handling
             //? int num1 = 20;
             //? int num2 = 5;
             //? System.out.println( "The addition of " + num1 + " and " + num2 + " is: " + c.sum(num1, num2));
 
-            ArrayList<CreditCard> items = (ArrayList<CreditCard>) c.getAll();
+            Scanner sc = new Scanner(System.in);
 
-            System.out.println(items.size());
+            while(true) {
+                System.out.println(" ____           _   _   __              _        _____          _      _                  _         ____       __     _ _ _        ");
+                System.out.println(" / ___| ___  ___| |_(_) /_/  _ __     __| | ___  |_   _|_ _ _ __(_) ___| |_ __ _ ___    __| | ___   / ___|_ __ /_/  __| (_) |_ ___  ");
+                System.out.println("| |  _ / _ \\/ __| __| |/ _ \\| '_ \\   / _` |/ _ \\   | |/ _` | '__| |/ _ \\ __/ _` / __|  / _` |/ _ \\ | |   | '__/ _ \\/ _` | | __/ _ \\ ");
+                System.out.println("| |_| |  __/\\__ \\ |_| | (_) | | | | | (_| |  __/   | | (_| | |  | |  __/ || (_| \\__ \\ | (_| |  __/ | |___| | |  __/ (_| | | || (_) |");
+                System.out.println(" \\____|\\___||___/\\__|_|\\___/|_| |_|  \\__,_|\\___|   |_|\\__,_|_| _/ |\\___|\\__\\__,_|___/  \\__,_|\\___|  \\____|_|  \\___|\\__,_|_|\\__\\___/ ");
+                System.out.println("                                                              |__/                                                                  ");
+
+                System.out.println("1. Leer Registros");
+                System.out.println("2. Crear Registro");
+                System.out.println("3. Actualizar Registro");
+                System.out.println("4. Eliminar Registro");
+
+                System.out.print("Introduzca una opción: ");
+                int option = sc.nextInt();
+
+                if(option == 1) {
+                    ArrayList<CreditCard> items = (ArrayList<CreditCard>) c.getAll();
+                    
+                    System.out.println("Cantidad de Registros: " + items.size());
+
+                    //? int id;
+                    //? String number;     //? Número de la tarjeta
+                    //? String holderDNI;  //? DNI del titular de la tarjeta
+                    //? String holderName; //? Nombre del titular de la tarjeta
+                    //? double balance;    //? Saldo actual
+
+                    if(items.isEmpty()) {
+                        System.out.println("¡No hay registros!");
+                    } else {
+                        for(int i = 0; i < items.size(); i++) {
+                            CreditCard item = items.get(i);
+    
+                            System.out.println("Registro #" + i + ":");
+                            System.out.println("id: " + item.id);
+                            System.out.println("Número de la tarjeta: " + item.number);
+                            System.out.println("DNI del titular de la tarjeta: " + item.holderDNI);
+                            System.out.println("Nombre del titular de la tarjeta: " + item.holderName);
+                            System.out.println("Saldo actual: " + item.balance);
+                        }
+                    }
+                } else if(option == 2) {
+
+                } else if(option == 3) {
+
+                } else if(option == 4) {
+
+                } else {
+                    System.out.println("¡Opción Inválida!");
+                }
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+            }
         } catch (MalformedURLException murle) {
             System.out.println();
             System.out.println("MalformedURLException");
