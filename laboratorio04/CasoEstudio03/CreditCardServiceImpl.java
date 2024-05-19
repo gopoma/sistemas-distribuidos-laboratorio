4a481793ab1cdc8622f61333623ca51c5e2d3fa4
gopoma <gordono@unsa.edu.pe>
2024-05-18 22:38:59 -0500

    Probando Excepciones Remotas

--- laboratorio04/CasoEstudio03/CreditCardServiceImpl.java
+++ laboratorio04/CasoEstudio03/CreditCardServiceImpl.java
@@ -6,6 +6,11 @@ public class CreditCardServiceImpl extends java.rmi.server.UnicastRemoteObject i
     }
 
     public ServerResponse sum(int a, int b) throws java.rmi.RemoteException {
+        boolean bad = true;
+
+        if(bad)
+            throw new java.rmi.RemoteException("dhuamanilu subirá a Expert finalizando mayo");
+
         return new ServerResponse(true, a + b);
     }
 }
